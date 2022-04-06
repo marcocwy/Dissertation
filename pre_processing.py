9beff09adfd9682a7917a39678a0092c18ac5148
marcocwy <marcocheung924@gmail.com>
2022-04-07 00:21:20 +0100

    Update pre_processing.py
    adding cleaning function, remove NaN (Not finished yet)

--- pre_processing.py
+++ pre_processing.py
@@ -7,7 +7,22 @@ class Pre_processing:
         self.df = df
         self.headings = list(self.df.columns.values)
         self.headings.pop(0) #removing first item in list, which is closing, as it does not need to be normalised
-    def normalisation(self):
+
+    def clean_df(self):
+        cleaned = self.df
+        # cleaned['Date'] = cleaned.index
+        # cleaned.dropna(inplace=True)
+        # cleaned.reset_index(drop=True, inplace=True)
+        
+        # # cleaned.set_index('Date',  inplace=True)
+        # self.df = cleaned
+        # print(self.df)
+
+        # for row in cleaned.index:
+            
+        # return cleaned
+
+    def normalisation(self): #put values between 0 and 1
 
         for col in self.headings:
             self.df[col] = self.df[col] / self.df[col].sum()
