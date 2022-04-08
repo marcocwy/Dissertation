46fff195698f965d3bcd76f9c402ab72afb68e0a
marcocwy <marcocheung924@gmail.com>
2022-04-08 17:29:45 +0100

    PCA + clarity in code
    PCA done, improved clarity in code

--- dimensionality_reduction.py
+++ dimensionality_reduction.py
@@ -8,42 +8,42 @@ class Dimensionality_reduction:
 
     def pca(self, n):
         
-        data_set = self.df.drop(columns=['Close'])
+        df = self.df.drop(columns=['Close'])
 
-        target = self.df['Close']
+        target = pd.DataFrame(self.df['Close'])
 
-        # url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-        # data = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
-        
-        # #prepare the data
-        # data_set = data.iloc[:,0:4] #pandas frame
-        # print(type(data_set))
-
-        # #prepare the target
-        # target = data.iloc[:,4]
-        # print(type(target)) #pandas series
 
         #centering
-        data_set_centered = data_set - np.mean(data_set, axis = 0) # mean:0, devi:1
+        df_centered = df - np.mean(df, axis = 0) # mean:0, devi:1
         
         #calculating covariance matrix, eigen matrix and eigen vectors
-        cov_matrix = np.cov(data_set_centered, rowvar = False)
-        # print(cov_matrix)
-        # eigen_values, eigen_vectors = np.linalg.eigh(cov_matrix)
+        cov_mat = np.cov(df_centered, rowvar = False)
+        eigen_values, eigen_vectors = np.linalg.eigh(cov_mat)
         
-        # #sorting eigenvalues in descending order
-        # sorted_index = np.argsort(eigen_values)[::-1]
-        # sorted_eigenvalue = eigen_values[sorted_index]
-        # sorted_eigenvectors = eigen_vectors[:,sorted_index]
+        #sorting eigenvalues in descending order
+        sorted_index = np.argsort(eigen_values)[::-1]
+        sorted_eigenvalue = eigen_values[sorted_index]
+        sorted_eigenvectors = eigen_vectors[:,sorted_index]
         
-        # #selecting the first n components
-        # n_eigenvectors = sorted_eigenvectors[:,0:n]
+        #selecting the first n components
+        n_eigenvectors = sorted_eigenvectors[:,0:n]
         
         # #transforming data to new labels
-        # data_set_transformed = np.dot(n_eigenvectors.transpose(), data_set_centered.transpose()).transpose()
+        data_set_transformed = np.dot(n_eigenvectors.transpose(), df_centered.transpose()).transpose()
         
         # print(data_set_transformed)
-        # # # return data_set_transformed
+        headings = []
+        for i in range(n):
+            heading = "PC" + str(i+1)
+            headings += [heading]
+        # print(headings)
+        pc_vals = pd.DataFrame(data_set_transformed, index = target.index ,columns = headings)
+        # print(principal_df)
+
+        pc_df = pd.concat([target, pc_vals] , axis = 1)
+        # print(pc_df)
+
+        self.df = pc_df
 
     def kpca(data_set, n):
         data_set_transformed = data_set
--- main.py
+++ main.py
@@ -9,19 +9,24 @@ import yfinance as yf
 
 class Main:
     
-    def __init__(self, stock_name, normalisation=False, savgol=False):
+    def __init__(self, stock_name, normalisation=False, savgol=False, PCA=0):
+        
+        ############################## Getting Historical data ##############################
+        
         # self.file = pd.read_csv(file, sep='\t')
-
         ticker = yf.Ticker(stock_name).history(period="max") #retrieving all historical market data
-        closing = ticker[['Close']] # extracting closing market value for the stock
+        self.df = ticker[['Close']] # extracting closing market value for the stock
 
-        df = dataframe.Dataframe(closing) # creating 
+        ############################## Populating dataframe ##############################
+
+        df = dataframe.Dataframe(self.df) # creating 
         df.populate_data_frame()
-        stock = df.df
-        # print(msft)
+        self.df = df.df
+        # print(self.df)
 
-        # #preprocess file
-        pp = pre_processing.Pre_processing(stock)
+        ############################## Preprocessing dataframe ##############################
+
+        pp = pre_processing.Pre_processing(self.df)
 
         pp.clean_df()
         # print(pp.df)
@@ -32,13 +37,22 @@ class Main:
 
         if savgol:
             pp.savitzky_golay(5, 2)
-        print(pp.df)
+        # print(pp.df)
+
+        self.df = pp.df
+        # print(self.df)
         
-        # #dimensionality reduction
-        # dr = dimensionality_reduction.Dimensionality_reduction(msft)
-        # pca = dr.pca(10)
+        ############################## Dimensionality Reduction ##############################
+
+        dr = dimensionality_reduction.Dimensionality_reduction(self.df)
+
+        if PCA > 0:
+            dr.pca(PCA)
+        print(dr.df)
+
         # kpca = Dimensionality_reduction.kpca(x, 10)
         
+
         # #Training
         # svr_model = Ml_algorithms.svr(file)
         # knn_model = Ml_algorithms.knn(file)
@@ -50,4 +64,4 @@ class Main:
         # #evaluation
         # Evaluation.mae()
 
-Main("MSFT", normalisation=False, savgol=True)
+Main("MSFT", normalisation=False, savgol=True, PCA=10)
