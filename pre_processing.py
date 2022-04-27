b5f27820e111d13690dc73a1f656391a5cde4676
marcocwy <marcocheung924@gmail.com>
2022-04-28 00:06:19 +0100

    Update pre_processing.py
    moved normalisation and savgol

--- pre_processing.py
+++ pre_processing.py
@@ -5,8 +5,6 @@ class Pre_processing:
 
     def __init__(self, df):
         self.df = df
-        self.headings = list(self.df.columns.values)
-        self.headings.pop(0) #removing first item in list, which is closing, as it does not need to be normalised
 
     def clean_df(self):
         '''
@@ -22,24 +20,4 @@ class Pre_processing:
         self.df.drop(self.df.index[contain_nan], inplace=True)
         # print(self.df)
         return self.df
-
-    def normalisation(self): #put values between 0 and 1
-        '''
-        normalise data to 0 and 1
-        '''
-        # print("Normalising data frame...")
-        for col in self.headings:
-            self.df[col] = self.df[col] / self.df[col].sum()
-        
-        
-
-    def savitzky_golay(self, n, order):  # n:odd, >=3, order <= n - 2
-        '''
-        savitzky golay smoothing filter
-        '''
-        # print("Applying savitzky_golay filter to data frame...")
-        for col in self.headings:
-            vals = self.df[col].tolist()
-            savgol = s.savgol_filter(vals, n, order).tolist()
-            self.df[col] = savgol
 
